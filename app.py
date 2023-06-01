9b64dce8c94c2f8a186372d28bc11bcff57bfb68
Waleed Murad <126694427+waleedmurad91@users.noreply.github.com>
2023-06-01 09:46:47 +0500

    app.py

--- app.py
+++ app.py
@@ -2,36 +2,49 @@ import streamlit as st
 import requests
 from bs4 import BeautifulSoup
 
-# Function to scrape car listings from PakWheels
-def scrape_pakwheels():
-    url = "https://www.pakwheels.com/used-cars/family-cars/587667"
-    response = requests.get(url)
-    soup = BeautifulSoup(response.content, 'html.parser')
-    
-    # Extract car listings from the website
-    listings = soup.find_all('li', class_='classified-listing')
-    data = []
-    for listing in listings:
-        title = listing.find('h3', class_='title').text.strip()
-        price = listing.find('span', class_='price').text.strip()
-        data.append({'Title': title, 'Price': price})
-    
-    return data
+# Define the base URL
+BASE_URL = "https://quotes.toscrape.com/"
 
-# Streamlit app code
+# Function to scrape the website
+def scrape_quotes():
+    # Send a GET request to the website
+    response = requests.get(BASE_URL)
+
+    # Parse the HTML content using BeautifulSoup
+    soup = BeautifulSoup(response.content, "html.parser")
+
+    # Find all quote elements on the page
+    quote_elements = soup.find_all("div", class_="quote")
+
+    # Extract the data from each quote element
+    quotes_data = []
+    for quote_element in quote_elements:
+        text = quote_element.find("span", class_="text").get_text()
+        author = quote_element.find("small", class_="author").get_text()
+        tags = [tag.get_text() for tag in quote_element.find_all("a", class_="tag")]
+
+        quotes_data.append({
+            "text": text,
+            "author": author,
+            "tags": tags
+        })
+
+    return quotes_data
+
+# Create a Streamlit web interface
 def main():
-    st.title("PakWheels Car Listings Scraper")
-    st.write("Click the button to scrape car listings from PakWheels:")
-    
-    if st.button("Scrape"):
-        try:
-            data = scrape_pakwheels()
-            st.write("Scraped car listings:")
-            for listing in data:
-                st.write(f"Title: {listing['Title']}, Price: {listing['Price']}")
-        except:
-            st.write("An error occurred while scraping data. Please try again.")
+    st.title("Quotes Scraper")
+    st.write("Scraping quotes from https://quotes.toscrape.com/")
+
+    # Scrape the website
+    quotes = scrape_quotes()
+
+    # Display the scraped quotes
+    for quote in quotes:
+        st.write("---")
+        st.write(f"Text: {quote['text']}")
+        st.write(f"Author: {quote['author']}")
+        st.write(f"Tags: {', '.join(quote['tags'])}")
 
 if __name__ == "__main__":
     main()
-
