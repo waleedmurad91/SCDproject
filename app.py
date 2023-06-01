94704db34d633ffed31ab77d04fedb67e7b8fe3a
Waleed Murad <126694427+waleedmurad91@users.noreply.github.com>
2023-06-01 12:53:02 +0500

    app.py

--- app.py
+++ app.py
@@ -8,29 +8,36 @@ BASE_URL = "https://quotes.toscrape.com/"
 
 # Function to scrape the website
 def scrape_quotes():
-    # Send a GET request to the website
+    # Send a GET request to the first page
     response = requests.get(BASE_URL)
-
-    # Parse the HTML content using BeautifulSoup
     soup = BeautifulSoup(response.content, "html.parser")
 
-    # Find all quote elements on the page
-    quote_elements = soup.find_all("div", class_="quote")
+    # Find the total number of pages
+    num_pages = len(soup.select(".pager a"))
 
-    # Extract the data from each quote element
+    # Scrape quotes from each page
     quotes_data = []
     tags_data = []
-    for quote_element in quote_elements:
-        text = quote_element.find("span", class_="text").get_text()
-        author = quote_element.find("small", class_="author").get_text()
-        tags = [tag.get_text() for tag in quote_element.find_all("a", class_="tag")]
-        quotes_data.append({
-            "text": text,
-            "author": author,
-            "tags": tags,
-        })
-        tags_data.extend(tags)
-    
+    for page in range(1, num_pages + 1):
+        url = f"{BASE_URL}page/{page}/"
+        response = requests.get(url)
+        soup = BeautifulSoup(response.content, "html.parser")
+
+        # Find all quote elements on the page
+        quote_elements = soup.find_all("div", class_="quote")
+
+        # Extract the data from each quote element
+        for quote_element in quote_elements:
+            text = quote_element.find("span", class_="text").get_text()
+            author = quote_element.find("small", class_="author").get_text()
+            tags = [tag.get_text() for tag in quote_element.find_all("a", class_="tag")]
+            quotes_data.append({
+                "text": text,
+                "author": author,
+                "tags": tags,
+            })
+            tags_data.extend(tags)
+
     top_tags = Counter(tags_data).most_common(10)
     return quotes_data, top_tags
 
@@ -49,8 +56,8 @@ def main():
         st.write(f"Author: {quote['author']}")
         st.write(f"Tags: {', '.join(quote['tags'])}")
 
-    # Display the top tags in the sidebar
-    st.sidebar.title("Top Tags")
+    # Display the overall top tags in the sidebar
+    st.sidebar.title("Overall Top Tags")
     for tag, count in top_tags:
         st.sidebar.write(f"{tag}: {count}")
 
